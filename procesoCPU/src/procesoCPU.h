0155640a53ca50d80744c29194c64ee3c4826e6e
MatiasMorsa <matimorsa@hotmail.com>
2015-11-19 13:06:35 -0300

    Fixes varios

--- procesoCPU/src/procesoCPU.h
+++ procesoCPU/src/procesoCPU.h
@@ -30,20 +30,23 @@
 /////////////////////////////////////////////////////VARIABLES///////////////////////////////////////
 
 
+
+char ipmem[15];
+char ipplanif[15];
+
 pthread_mutex_t mutex;
 t_log* logger;
 t_config* cfg;
 
-//int socket_planif;
-//int socket_mem;
-
 int* socket_planificador;
-int socket_planificador_especial;
 int* socket_memoria;
-
-
 int* sentencias_ejecutadas_ultimo_min;
 int* porcentaje_a_planificador;
+
+int socket_planificador_especial;
+int puertomem;
+int cant_hilos;
+int puertoplanif;
 
 /////////////////////////////////////////////////ESTRUCTURAS/////////////////////////////////////////
 
@@ -65,6 +68,7 @@ typedef struct{
 	bool ejecuto_ok;
 	t_list* resultados_sentencias;
 }t_resultado_pcb;
+
 ///////////////////////////////////////////////PROTOTIPOS//////////////////////////////////////////////
 
 int inicializar();
@@ -72,31 +76,37 @@ int finalizar();
 int conectar_con_planificador(int numero);
 int conectar_con_planificador_especial();
 int enviar_porcentaje_a_planificador();
-t_msg* sent_to_msg(t_sentencia* sent);
-int enviar_logs(int socket, t_list* resultados_sentencias);
-void* hilo_responder_porcentaje();
-void inicializar_porcentajes();
-void inicializar_resultados_sentencias();
-void* hilo_porcentaje() ;
-int procesar_mensaje_planif(t_msg* msg,int numero);
-t_resultado_pcb ejecutar(t_pcb* pcb,int socket_mem, int hilo);
-int procesar_mensaje_planif(t_msg* msg,int numero);
-//t_resultado_pcb ejecutar(t_pcb* pcb,int numero);
-int pcb_tiene_que_seguir_ejecutando(t_pcb* pcb);
-int sent_ejecutar_iniciar(t_sentencia* sent,int socket_mem);
-char* sent_ejecutar_leer(t_sentencia* sent,int socket_mem);
-int sent_ejecutar_finalizar(t_sentencia* sent,int socket_mem);
-int conectar_con_memoria(int numero);
-void* hilo_cpu(int *numero_hilo);
-char* IP_PLANIFICADOR();
-int PUERTO_PLANIFICADOR();
-char* IP_MEMORIA();
 int PUERTO_MEMORIA();
 int CANTIDAD_HILOS();
 int RETARDO();
 int RETARDO_MINIMO();
-
 int avisar_a_planificador(t_resultado_pcb respuesta,int socket_planif, int hilo);
+int enviar_logs(int socket, t_list* resultados_sentencias);
+int procesar_mensaje_planif(t_msg* msg,int numero);
+int procesar_mensaje_planif(t_msg* msg,int numero);
+int pcb_tiene_que_seguir_ejecutando(t_pcb* pcb);
+int sent_ejecutar_iniciar(t_sentencia* sent,int socket_mem);
+int PUERTO_PLANIFICADOR();
+int sent_ejecutar_finalizar(t_sentencia* sent,int socket_mem);
+int conectar_con_memoria(int numero);
+
+t_msg* sent_to_msg(t_sentencia* sent);
+t_resultado_pcb ejecutar(t_pcb* pcb,int socket_mem, int hilo);
+
+
+void* hilo_responder_porcentaje();
+void* hilo_cpu(int *numero_hilo);
+void* hilo_porcentaje() ;
+
+void inicializar_porcentajes();
+void inicializar_resultados_sentencias();
+
+char* CONFIG_PAT();
+char* LOGGER_PAT();
+char* sent_ejecutar_leer(t_sentencia* sent,int socket_mem);
+char* IP_PLANIFICADOR();
+char* IP_MEMORIA();
+
 
 
 
