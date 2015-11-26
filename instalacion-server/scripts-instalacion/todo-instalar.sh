9a4810c3fda38add739c884dd35b31d0fd7f94c9
PabloMartinP <pablo.penialoza@gmail.com>
2015-11-26 09:02:51 -0300

    Create todo-instalar.sh

---
+++ instalacion-server/scripts-instalacion/todo-instalar.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/utnso/Escritorio/git/tp-2015-2c-killthepony/instalacion-server/scripts-instalacion/
+source procesoAdminMem-instalar.sh
+
+cd /home/utnso/Escritorio/git/tp-2015-2c-killthepony/instalacion-server/scripts-instalacion/
+source procesoCPU-instalar.sh
+
+cd /home/utnso/Escritorio/git/tp-2015-2c-killthepony/instalacion-server/scripts-instalacion/
+source procesoPlanificador-instalar.sh
+
+cd /home/utnso/Escritorio/git/tp-2015-2c-killthepony/instalacion-server/scripts-instalacion/
+source procesoSwap-instalar.sh
+
+cd /home/utnso/Escritorio/git/tp-2015-2c-killthepony/instalacion-server/scripts-instalacion/
+source utiles-instalar.sh
+
+
+
+echo "***************Fin todo OKOKOKOKOKOKOKOKOKOKOKOKOKOKOKOK "
